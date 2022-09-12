da58b82c48849b00055e0a772edf2d98f52f31f3
Sharif Kamran <100826608+sharif8092@users.noreply.github.com>
2022-09-12 12:12:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+import slides from "../data.js";
+import '../styles/App.css'
 
-const App = () => {
+const App = (props) => {
+   const [data , setdata] = useState(0);
+
+   function nextSlide(){
+    // if(data<slides.length-1)
+    {setdata(data+1)}
+
+   }
+   function prevSlide(){
+    // if(data>=1)
+    {
+    setdata(data-1)};
+
+   }
   return (
-    <></>
+    
+    <>
+    
+     <p>{slides[data].title}</p>
+    <button onClick={nextSlide} >Next</button>
+    <button onClick={prevSlide} disabled={data<1 ? "true" : "false"}>Prev</button>
+  
+    
+  
+    
+    </>
   )
 }
 
